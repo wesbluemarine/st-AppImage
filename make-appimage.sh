c0857e102077cf9464ebb28cd50300cb9e7edbfd
Alberto <14043560+wesbluemarine@users.noreply.github.com>
2026-03-03 16:57:28 +0100

    Change application version retrieval from 'st' to 'tint2'

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,18 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q st | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q tint2 | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/702499f331aa9c38309e1af99de4021013916297/Papirus/64x64/apps/st.svg
 export DEPLOY_OPENGL=0
 export DEPLOY_VULKAN=0
 export ANYLINUX_LIB=1
 export URUNTIME_PRELOAD=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/st
+quick-sharun /usr/bin/tint2
 
 # Additional changes can be done in between here
 
